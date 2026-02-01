8ec5e5a3604c2f83d7769cb3ab1782e1ef04db97
nothke <nothke@gmail.com>
2026-02-01 16:35:27 +0100

    Add mask fix

--- Global.gd
+++ Global.gd
@@ -18,12 +18,4 @@ func add_fighter(dict: Dictionary[String, int], texture, enemy_texture):
 
 func add_mask(mask_res: MaskResource):
 	# OVDE TREBA DA SE STAVI MASKA
-	
-	var enemy_child = ENEMY.instantiate()
-	enemy_child.damage = dict["damage"]-5
-	enemy_child.attack_speed = dict["attack_speed"]
-	enemy_child.health = dict["health"]
-	enemy_child.max_health = dict["max_health"]
-	enemy_child.range = range
-	enemy_child.texture = texture
-	enemies.append(enemy_child)
+	pass
